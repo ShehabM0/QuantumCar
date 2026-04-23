1fab51001c41e5f70899f7e157b507eee4de54f9
ShehabM0 <shehabmohamed2357@gmail.com>
2026-04-23 17:18:01 +0200

    add car engine replacement

--- src/Car.java
+++ src/Car.java
@@ -5,6 +5,17 @@ public class Car {
         this.engine = engine;
     }
 
+    public void replaceEngine(Engine engine) {
+        String engineType = this.engine.getType();
+        String targetEngineType = engine.getType();
+        if(engineType.equals(targetEngineType))
+            System.out.println("Car is already on " + engineType);
+        else {
+            this.engine = engine;
+            System.out.println("Car engine has been switched to " + engineType);
+        }
+    }
+
     public void getEngineType() {
         System.out.println(engine.getType());
     }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Car gasolineCar = new Car(new GasolineEngine());
-        gasolineCar.getEngineType();
+        Car car = new Car(new GasolineEngine());
+        car.replaceEngine(new GasolineEngine());
 
-        Car electronicCar = new Car(new ElectronicEngine());
-        electronicCar.getEngineType();
-
-        Car hybridCar = new Car(new HybridEngine());
-        hybridCar.getEngineType();
+        car.replaceEngine(new GasolineEngine());
+        car.replaceEngine(new ElectronicEngine());
+        car.replaceEngine(new ElectronicEngine());
+        car.replaceEngine(new HybridEngine());
     }
 }
